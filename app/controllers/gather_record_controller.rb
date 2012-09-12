1a16992c4efc2a314b956fd39640738bc81c0108
Paul Scarrone <paul.scarrone@gmail.com>
2012-09-11 20:35:17 -0400

    added entity generation on request to object

--- app/controllers/gather_record_controller.rb
+++ app/controllers/gather_record_controller.rb
@@ -2,11 +2,31 @@ class GatherRecordController < ApplicationController
   def gather
     
     submitHash = request.GET
-    submitHash[:host]
-    host = Host.new
-    host.domain = submitHash[:host]
-    host.uri = submitHash[:uri]
-    host.save
+    
+    #HEADER Object
+    head = Header.new
+    #END HEADER
+    
+    #CORDINATE Object
+    cord = Cordinate.new
+    cord.mouse submitHash[:mouse]
+    cord.screen submitHash[:screen]
+    #END CORDINATE
+    
+    #RECORD_STORAGE Object
+    rs = RecordStorage.new
+    rs.cordinate = cord
+    rs.headers.push
+    #END RECORD_STORAGE
+    
+    #HOST object
+    @host = Host.new
+    @host.domain = submitHash[:host]
+    @host.uri = submitHash[:uri]
+    @host.record_storage = rs
+    @host.save
+    #END HOST
+    
     respond_to do |format|
       format.html
       format.json{
