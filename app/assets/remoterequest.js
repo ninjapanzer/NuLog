bbd96066dfee8f8cf08192d1dd6461cbf5721df8
Paul Scarrone <paul.scarrone@gmail.com>
2012-09-17 20:35:58 -0400

    Sample include script for event triggers

---
+++ app/assets/remoterequest.js
@@ -0,0 +1,26 @@
+function getJSON(URL,success){
+    var ud = 'json'+(Math.random()*100).toString().replace(/\./g,'');
+    window[ud]= function(o){
+        success&&success(o);
+    };
+    document.getElementsByTagName('body')[0].appendChild((function(){
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.src = URL.replace('callback=?','callback='+ud);
+        return s;
+    })());
+}
+
+function setclick(evt){
+	evt.preventDefault();
+	console.log(evt);
+	getJSON("http://localhost:3000/submit?mouse="+evt.clientX+","+evt.clientY+"&screen="+window.innerWidth+","+window.innerHeight+"&host="+window.location.origin+"&uri="+escape(window.location.pathname)+"&callbakc=?",null);
+}
+
+if(document.addEventListener) { // DOM standard
+    document.addEventListener('click', setclick, false);
+	document.addEventListener('hashchange', setclick, false);
+} else if(document.attachEvent) { // IE
+    document.attachEvent('onclick', setclick);
+	document.attachEvent('onhashchange', setclick);
+}
